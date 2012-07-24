d2cf9f1c184a3443b3b2d9ba06b95c3a26fb42f9
Benjamin James Harrison-Sims <tehrainbowguy@gmail.com>
2012-07-24 17:43:45 +0100

    flame on.

--- src/com/xegaming/xTroll/xListener.java
+++ src/com/xegaming/xTroll/xListener.java
@@ -1,6 +1,12 @@
 package com.xegaming.xTroll;
 
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * User: Benjamin
@@ -12,6 +18,37 @@ public class xListener implements Listener {
 
     public xListener(xTroll XE) {
         this.XE = XE;
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+            final Player player = event.getPlayer();
+            ItemStack stack = player.getItemInHand();
+            if (xTroll.permission.has(player, "XT.flame") || player.isOp()) {
+                if (stack.getType().equals(Material.TORCH)) {
+                    final Location location = player.getEyeLocation();
+                    final World world = player.getWorld();
+                    if (player.isSneaking()) {
+                        for (int i = 1; i <= 5; i++) {
+                            final int finalI = i;
+                            Bukkit.getScheduler().scheduleSyncDelayedTask(XE, new Runnable() {
+                                public void run() {
+                                    Location newloc = location.toVector().add(location.getDirection().multiply(3 * finalI)).toLocation(player.getWorld());
+                                    world.playEffect(newloc, Effect.MOBSPAWNER_FLAMES, 0);
+                                }
+                            }, 5L);
+                        }
+
+
+                    } else {
+                        Location newloc = location.toVector().add(location.getDirection().multiply(3)).toLocation(player.getWorld());
+                        world.playEffect(newloc, Effect.MOBSPAWNER_FLAMES, 0);
+                    }
+                }
+            }
+        }
+
     }
 
 
